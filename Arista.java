70f343ba5c570f82c6f340f8c94f49cc61e6bcdf
Richixs <ricardoramirezcaero123@gmail.com>
2024-09-09 01:03:23 -0400

    refactor: logica clase Arista

--- Arista.java
+++ Arista.java
@@ -5,31 +5,18 @@ public class Arista {
     private ArrayList arista;
     private Vertice[] vertices;
     private int peso;
-    private boolean dirigido;
 
     public Arista(Vertice verticeA, Vertice verticeB) {
         this.arista = new ArrayList<>(4);
         vertices = new Vertice[] {verticeA, verticeB};
-        this.dirigido = false;
         this.peso = 0;
         arista.add(0, vertices);
-        arista.add(1, this.dirigido);
-        arista.add(2, peso);
-    }
-
-    public Arista(Vertice verticeA, Vertice verticeB, boolean dirigido) {
-        this(verticeA, verticeB);
-        this.dirigido = dirigido;
+        arista.add(1, peso);
     }
 
     public Arista(Vertice verticeA, Vertice verticeB, int peso) {
         this(verticeA, verticeB); 
         this.peso = peso;
-    }
-
-    public Arista(Vertice verticeA, Vertice verticeB, int peso, boolean dirigido) {
-        this(verticeA, verticeB, peso); 
-        this.dirigido = dirigido;
     }
 
     public ArrayList getArista() {
