4af30e23b0c77b147ca4afb21d170e91781616c7
Richixs <ricardoramirezcaero123@gmail.com>
2024-09-09 02:32:02 -0400

    feature: Grafo - printEmparejamientos()

--- Grafo.java
+++ Grafo.java
@@ -1,4 +1,6 @@
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 public class Grafo {
     private ArrayList<Vertice> verticeList;
@@ -17,34 +19,13 @@ public class Grafo {
         this.aristaList = aristaList;
     }
 
-    public void printGrafo() {
-        System.out.println("Grafo - " + (this.dirigido ? "Dirigido" : "No Dirigido"));
-        for (Vertice vertice : verticeList) {
-            System.out.print(vertice.getIdVertice() + ": ");
-            ArrayList<String> adyacentes = new ArrayList<>();
-            for (Arista arista : aristaList) {
-                String verticeA = arista.getVertice(false).getIdVertice();
-                String verticeB = arista.getVertice(true).getIdVertice();
-                int peso = (int) arista.getArista().get(1);
-                if (vertice.getIdVertice().equals(verticeA)) {
-                    adyacentes.add(verticeB + (peso != 0 ? " (Peso: " + peso + ")" : ""));
-                }
-                if (!dirigido && vertice.getIdVertice().equals(verticeB)) {
-                    adyacentes.add(verticeA + (peso != 0 ? " (Peso: " + peso + ")" : ""));
-                }
-            }
-            System.out.println(String.join(", ", adyacentes));
-        }
-    }
-    
-
     public boolean addVertice(Vertice nuevoVertice) {
         try {
             this.verticeList.add(nuevoVertice);
             return true;
         } catch (Exception e) {
             System.out.println("No se puedo aniadir el vertice: " +
-                nuevoVertice.getIdVertice() +
+                nuevoVertice +
                 "\n se capturo la excepcion: " +
                 e.getMessage());
             return false;
@@ -57,8 +38,8 @@ public class Grafo {
             return true;
         } catch (Exception e) {
             System.out.println("No se pudo añadir la arista: [" + 
-                nuevaArista.getVertice(false).getIdVertice() + ", " + 
-                nuevaArista.getVertice(true).getIdVertice() + "]\n" + 
+                nuevaArista.getVertice(false) + ", " + 
+                nuevaArista.getVertice(true) + "]\n" + 
                 "Se capturó la excepción: " + e.getMessage());
             return false;
         }
@@ -66,5 +47,79 @@ public class Grafo {
 
     public ArrayList<Vertice> getverticeList() {
         return this.verticeList;
+    }
+
+    public void printGrafo() {
+        System.out.println("Grafo - " + (this.dirigido ? "Dirigido" : "No Dirigido"));
+        for (Vertice vertice : verticeList) {
+            System.out.print(vertice + ": ");
+            ArrayList<String> adyacentes = new ArrayList<>();
+            for (Arista arista : aristaList) {
+                String verticeA = arista.getVertice(false).toString();
+                String verticeB = arista.getVertice(true).toString();
+                int peso = (int) arista.getArista().get(1);
+                if (vertice.toString().equals(verticeA)) {
+                    adyacentes.add(verticeB + (peso != 0 ? " (Peso: " + peso + ")" : ""));
+                }
+                if (!dirigido && vertice.toString().equals(verticeB)) {
+                    adyacentes.add(verticeA + (peso != 0 ? " (Peso: " + peso + ")" : ""));
+                }
+            }
+            System.out.println(String.join(", ", adyacentes));
+        }
+    }
+
+     public void printEmparejamientos() {
+        ArrayList<Set<Vertice>> emparejamientos = new ArrayList<>();
+        Set<Vertice> emparejamientoActual = new HashSet<>();
+        for (Arista arista : aristaList) {
+            Vertice v1 = arista.getVertice(false);
+            Vertice v2 = arista.getVertice(true);
+            if (!emparejamientoActual.contains(v1) && !emparejamientoActual.contains(v2)) {
+                Set<Vertice> par = new HashSet<>();
+                par.add(v1);
+                par.add(v2);
+                emparejamientos.add(par);
+                emparejamientoActual.add(v1);
+                emparejamientoActual.add(v2);
+            }
+        }
+        System.out.println("Emparejamientos:");
+        for (Set<Vertice> par : emparejamientos) {
+            System.out.println(par);
+        }
+        boolean esMaximo = emparejamientoActual.size() == verticeList.size();
+        boolean esMaximal = esEmparejamientoMaximal(emparejamientos);
+        boolean esPerfecto = emparejamientoActual.size() == verticeList.size() && emparejamientoActual.size() % 2 == 0;
+        System.out.println("Tipo de emparejamiento:");
+        if (esPerfecto) {
+            System.out.println("Emparejamiento perfecto");
+        } else if (esMaximo) {
+            System.out.println("Emparejamiento máximo");
+        } else if (esMaximal) {
+            System.out.println("Emparejamiento maximal");
+        } else {
+            System.out.println("Emparejamiento no es maximal ni máximo");
+        }
+    }
+
+    private boolean esEmparejamientoMaximal(ArrayList<Set<Vertice>> emparejamientos) {
+        Set<Vertice> emparejados = new HashSet<>();
+        for (Set<Vertice> par : emparejamientos) {
+            emparejados.addAll(par);
+        }
+        for (Vertice v : verticeList) {
+            if (!emparejados.contains(v)) {
+                for (Arista arista : aristaList) {
+                    if (arista.getVertice(false).equals(v) || arista.getVertice(true).equals(v)) {
+                        Vertice otro = arista.getVertice(false).equals(v) ? arista.getVertice(true) : arista.getVertice(false);
+                        if (!emparejados.contains(otro)) {
+                            return false;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
